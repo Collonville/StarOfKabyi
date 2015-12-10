1c8c79332bdc5aea2c9405a420f09ab403cfe462
ketsuaza <ketsuaza@gmail.com>
2015-12-11 02:05:55 +0900

    車選択の実装
    選択用のtrigger付きオブジェクトの実装。
    
    このtriggerの中に入っていて、かつタグが"vehicle"に設定されているゲームオブジェクトが存在するとき、ENTERキーを押すと候補のゲームオブジェクトの中で最も近いオブジェクトからエフェクトが出る。
    
    エフェクトが出ている間に移動すると最も近いオブジェクトにエフェクトが移る。範囲から一つもオブジェクトがなくなると選択は解除され、再びENTERを押すまでエフェクトは出ない。
    これをPlayerに子オブジェクトとして装着する。（テスト用に、Start時に自動で子オブジェクトに設定して操作できるPlayerを作った）

---
+++ Assets/Scripts/select_area.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class select_area : MonoBehaviour
+{
+    /// <summary>
+    /// 選択できる範囲のためのスクリプト。
+    /// 範囲に入ったオブジェクトをリストに保存して、リストをUpdate中で回して距離を確認する。
+    /// この範囲から出ていったオブジェクトはリストから削除する。
+    /// 範囲の中にオブジェクトがあるとき、Enterキーを押すと、プレイヤーから最も近いコライダーとrigitbodyが付いたオブジェクトにエフェクトがでる。
+    /// エフェクトが出ているとき、範囲内に別のオブジェクトがあった場合、そのオブジェクトに近づくとエフェクトがそのオブジェクトに切り替わる。（選択対象が変わる）
+    /// 範囲外に出ると、選択は解除される。選択が解除されると、再びEnterを押すまでオブジェクトは選択されなくなる。
+    /// </summary>
+    public GameObject perticle;
+    GameObject nowPerticle;
+    GameObject nowSelected;
+    List<GameObject> intoObjectList = new List<GameObject>();
+    GameObject nearistObject;
+    GameObject decisionObject;
+    void Start()
+    {
+        nearistObject=null;
+        decisionObject = null;
+        nowSelected = null;
+        nowPerticle = null;
+    }
+    void Update()
+    {
+        float nearistDist=-1;
+        var myPosition = transform.position;
+        var isEnter = Input.GetKey(KeyCode.Return);
+        foreach (GameObject obj in intoObjectList)
+        {
+            if (nearistDist == -1)
+            {
+                nearistDist = Vector3.Distance(myPosition, obj.transform.position);
+                nearistObject = obj;
+                if (decisionObject != null)
+                {
+                    decideObject(true);
+                }
+            }
+            if (nearistDist>Vector3.Distance(myPosition, obj.transform.position))
+            {
+                nearistDist = Vector3.Distance(myPosition,obj.transform.position);
+                nearistObject = obj;
+                if (decisionObject != null)
+                {
+                    decideObject(true);
+                }
+            }
+        }
+        if (isEnter == true)
+        {
+            decideObject(true);
+        }
+        perticleManager();
+        Debug.Log(decisionObject);
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals("vehicle"))
+        {
+            intoObjectList.Add(collision.gameObject);
+        }
+       
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        intoObjectList.Remove(collision.gameObject);
+        if (collision.gameObject==nearistObject)
+        {
+            nearistObject = null;
+            decideObject(false);
+        }
+    }
+    void decideObject(bool isEnter)
+    {
+        if (isEnter)
+        {
+            if (nearistObject != null)
+            {
+                decisionObject = nearistObject;
+            }
+            else
+            {
+                decisionObject = null;
+            }
+        }
+        else
+        {
+            decisionObject = null;
+        }
+    }
+    void perticleManager()
+    {
+        Vector3 offset = new Vector3(0,3,0);
+        if (decisionObject!=null)
+        {
+            if (nowPerticle == null)
+            {
+                nowPerticle = (GameObject)Instantiate(perticle,decisionObject.transform.position+offset,Quaternion.Euler(270,0,0));
+                nowPerticle.transform.parent = decisionObject.transform;
+            }
+            if (nowSelected == null)
+            {
+                nowSelected = decisionObject;
+            }
+            if (nowSelected != decisionObject)
+            {
+                Destroy(nowPerticle);
+                nowPerticle = (GameObject)Instantiate(perticle, decisionObject.transform.position+offset,Quaternion.Euler(270, 0, 0));
+                nowPerticle.transform.parent = decisionObject.transform;
+            }
+            nowSelected = decisionObject;
+        }
+        else
+        {
+            if (nowPerticle != null)
+            {
+                Destroy(nowPerticle);
+                nowPerticle = null;
+            }
+            nowSelected = null;
+        }
+    }
+}
---
+++ Assets/Scripts/test_player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_player : MonoBehaviour
+{
+    public GameObject area;
+    void Start()
+    {
+        var myPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        var rotation = new Vector3(0,0,0);
+        var myArea=(GameObject)Instantiate(area,myPosition,Quaternion.Euler(rotation));
+        myArea.transform.parent = transform;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") / 5;
+        float y = 0;
+        float z = Input.GetAxis("Vertical") / 5;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            z = 0;
+            y = Input.GetAxis("Vertical") / 5;
+            Vector3 direction = transform.rotation.eulerAngles.normalized;
+            transform.position = new Vector3(transform.localPosition.x + x * direction.x, transform.localPosition.y + y * direction.y, transform.localPosition.z + z*direction.z);
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x + x*50, transform.rotation.eulerAngles.y + y*50, transform.rotation.eulerAngles.z + z*50);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.localPosition.x + x, transform.localPosition.y + y, transform.localPosition.z + z);
+        }
+    }
+}
