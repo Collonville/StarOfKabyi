84aa5fadb38fcd21cca9b6078dbd3ca01dd8d54a
ketsuaza <ketsuaza@gmail.com>
2016-02-12 19:02:55 +0900

    スクリプト
    スクリプト

---
+++ Assets/Scripts/return_button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class return_button : MonoBehaviour {
+    public void returnToMenu()
+    {
+        Application.LoadLevel("SimpleTown_DemoScene");
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/skydome.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skydome : MonoBehaviour {
+    /// <summary>
+    /// skydomeに取りつけ、空の色を切り替え、朝と夜を表現している。
+    /// </summary>
+    GameObject star;
+    star_rotate starClass;
+	// Use this for initialization
+
+	void Start () {
+        Vector2 offset = GetComponent<Renderer>().sharedMaterial.mainTextureOffset;
+        offset.x = 0;
+        offset.y = 0;
+        GetComponent<Renderer>().sharedMaterial.mainTextureOffset = offset;
+        star = gameObject.transform.FindChild("Stars").gameObject;
+        starClass = star.GetComponent<star_rotate>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = GetComponent<Renderer>().sharedMaterial.mainTextureOffset;
+        offset.x = Mathf.Repeat(offset.x + 0.0003f, 1.0f);
+        if (offset.x>0.35&&offset.x<0.7)
+        {
+            starClass.switchStar(true);
+        }
+        else
+        {
+            starClass.switchStar(false);
+        }
+        offset.y = 0;
+        GetComponent<Renderer>().sharedMaterial.mainTextureOffset = offset;
+    }
+}
